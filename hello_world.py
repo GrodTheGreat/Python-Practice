a550dfb4c9b2541ba316add5b7e948ee7866f197
Gerard Causse <grod.causse@gmail.com>
2024-06-08 12:21:58 -0400

    Add Hello World script, project completed

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+def main():
+    '''
+    This is just a Hello World script. This just makes sure that Python is
+    correctly installed and can run.
+    '''
+    print('Hello World!')
+
+if __name__ == '__main__':
+    main()
